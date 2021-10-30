596f92471cfecae14debcbcd3c09f674465fe8b9
MaxTGM <jaimaini3@gmail.com>
2021-10-30 16:49:08 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        'main': '2fr 1fr',
+  },
+  colors: {
+    primary:'#56CCF2',
+    secondary: '#5a4fcf',
+    third:'#F1E4FF',
+    fourth: '#18191F'
+  },
+  fontFamily: {
+    'body': [ 'Manrope', 'sans-serif'],
+   },
+   width: {
+    'body':'60%'
+   },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+}
+}
